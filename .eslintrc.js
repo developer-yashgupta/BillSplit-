09adc6af93c1a49dccd65373fc36818aae8a516c
Yash <yashgupta9437@gmail.com>
2025-12-17 23:01:25 +0530

    add ESLint configuration and .gitignore file

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    '@react-native-community',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'react-native/no-inline-styles': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+  },
+};
